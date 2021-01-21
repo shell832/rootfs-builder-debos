a992dac6fdfd03fa86b28137fe43921ee8ac6278
TheKit <nekit1000@gmail.com>
2021-01-21 21:28:47 +0200

    android9: add xenial_-_android9_-_android10 repo as well

--- scripts/add-android9-repos.sh
+++ scripts/add-android9-repos.sh
@@ -10,10 +10,15 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
 
 echo "deb http://repo.ubports.com/ xenial_-_android9 main" >> /etc/apt/sources.list.d/ubports-android9.list
+echo "deb http://repo.ubports.com/ xenial_-_android9_-_android10 main" >> /etc/apt/sources.list.d/ubports-android9.list
 
 echo "Package: *" >> /etc/apt/preferences.d/ubports-android9.pref
 echo "Pin: release o=UBports,a=xenial_-_android9" >> /etc/apt/preferences.d/ubports-android9.pref
 echo "Pin-Priority: 2010" >> /etc/apt/preferences.d/ubports-android9.pref
+
+echo "Package: *" >> /etc/apt/preferences.d/ubports-android9.pref
+echo "Pin: release o=UBports,a=xenial_-_android9_-_android10" >> /etc/apt/preferences.d/ubports-android9.pref
+echo "Pin-Priority: 2011" >> /etc/apt/preferences.d/ubports-android9.pref
 
 if [ "$CHANNEL" == "edge" ]; then
     echo "deb http://repo.ubports.com/ xenial_-_edge_-_android9 main" >> /etc/apt/sources.list.d/ubports-android9.list
@@ -26,7 +31,7 @@ fi
 apt update
 apt upgrade -y --allow-downgrades
 
-apt install -y bluebinder ofono-ril-binder-plugin pulseaudio-modules-droid-28
+apt install -y bluebinder ofono-ril-binder-plugin pulseaudio-modules-droid-28 pulseaudio-modules-droid-29
 # sensorfw
 apt remove -y qtubuntu-sensors
 apt install -y libsensorfw-qt5-hybris libsensorfw-qt5-configs libsensorfw-qt5-plugins libqt5sensors5-sensorfw qtubuntu-position
